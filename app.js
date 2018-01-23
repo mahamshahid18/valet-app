c073961cfc53b02129b9bdf1d94d17a83c4f4cd2
Maham Shahid <mahum.shahid934@gmail.com>
2018-01-23 19:08:08 +0500

    Cleaned up code, added tests for remaining endpoints, implemented code linting

--- app.js
+++ app.js
@@ -5,81 +5,98 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
-const twilio = require('twilio');
-const ngrok = require('ngrok');
+const Twilio = require('twilio');
+const helmet = require('helmet');
 
+// global configuration settings
 const dbConnectConfig = {
-	host: "",
-	port: "",
-	user: "root",
-	password: "",
-	database: "",
+	host: '',
+	port: '',
+	user: 'root',
+	password: '',
+	database: '',
 	connectionLimit: 0
 };
-let baseUrl = "";
+let baseUrl = '';
 
 const setConfigSettings = () => {
-	let configData = fs.readFileSync("config.json");
+	let configData = fs.readFileSync('config.json');
 	configData = JSON.parse(configData);
 
-	baseUrl = `${configData.baseUrl}:${configData.backendPort}`;
+	baseUrl = (configData.backendPort === null) || (configData.backendPort === '')
+				? `${configData.baseUrl}`
+				: `${configData.baseUrl}:${configData.backendPort}`;
+
 	dbConnectConfig.host = configData.baseUrl;
 	dbConnectConfig.port= configData.dbPort;
 	dbConnectConfig.user = configData.dbUserName;
 	dbConnectConfig.password = configData.dbPassword;
 	dbConnectConfig.database = configData.dbName;
 	dbConnectConfig.connectionLimit = configData.dbConnLimit;
-}
+};
 
 setConfigSettings();
+
 // start of the sequence number for generating tickets
-let seq_no = JSON.parse(fs.readFileSync('sequencer.json')).start;
+let seqNo = JSON.parse(fs.readFileSync('sequencer.json')).start;
 const pool = mysql.createPool(dbConnectConfig);
 
 const app = express();
-// support json encoded bodies
+// support encoded request bodies
 app.use(bodyParser.json());
-// support encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
-	ngrok.connect(3000, function (err, url) {});
+	res.send('Greetings, young one!');
 });
 
 app.listen(3000, () => {
-	console.log('Server started listening on port 3000');
+	console.log('Server started; listening on port 3000');
 });
 
-
+/**
+ * Endpoint to generate an e-ticket
+ *
+ * @name POST/generateTicket
+ *
+ * @param {string} name - Name of user
+ * @param {string} phone - User's cellphone number
+ * @param {string} reg_no - Car's registration number
+ * @param {string} color - Car color
+ * @param {string} model_make - Car's model & make
+ *
+ * Returns HTTP 200 OK status code
+ */
 app.route('/generateTicket')
 	.post((req, res) => {
 		const name = req.body.name;
-		const phone_number = req.body.phone;
-		const reg_no = req.body.reg_no;
+		const phoneNumber = req.body.phone;
+		const regNo = req.body.reg_no;
 		const color = req.body.color;
-		const model_make = req.body.model_make;
-		const ticket_no = `${seq_no}${reg_no}`;
-		// TODO: convert ticket_no to base64 before building link
+		const modelMake = req.body.model_make;
+		const ticketNum = `${seqNo}${regNo}`;
+		// TODO: convert ticketNum to base64 before building link
 		// TODO: convert base-url before shipping code
-		const ticket_link = `http://${dbConnectConfig.host}/valet/app/templates/#!/user?ticket=${ticket_no}`;
+		const ticketLink =
+			`http://${baseUrl}/valet/app/templates/#!/user?ticket=${ticketNum}`;
 
 		const query = 'INSERT INTO `users`\
 			VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-		const values = [name, phone_number, reg_no, color, model_make, ticket_no, 0, 5];
+		const values = [name,
+			phoneNumber,
+			regNo,
+			color,
+			modelMake,
+			ticketNum,
+			0,
+			5
+		];
 
-		pool.getConnection((err, connection) => {
-			if(err) {
-				throw err;
-			}
-			connection.query(query, values, queryHandler);
-			connection.release();
-		});
-
-		const queryHandler = (err, result) => {
-			if(err) {
-				// query failed
+		const queryHandler = (err) => {
+			if (err) {
+				// query failed, handle exception
 				const errorModel = {
 					errorCode: 500,
 					errorMessage: 'Error executing the query on db',
@@ -87,48 +104,62 @@ app.route('/generateTicket')
 				};
 				console.log(errorModel);
 			} else {
-				seq_no++;
+				seqNo++;
 
+				// add twilio app credentials
 				const accountSid = 'ACe95761f79c6402d49380108bdba3be0c';
 				const authToken = '73cd5dd816f8ccbe3bde5ec5c21a6e35';
+				const twilioPhoneNumber = '+18589430166';
 
-				const client = new twilio(accountSid, authToken);
+				const client = new Twilio(accountSid, authToken);
 
+				// send ticket link to user via sms
 				client.messages.create({
-				    body: `Open up this link to view your valet ticket: ${ticket_link}`,
-				    to: phone_number,
-				    from: '+18589430166 '
+					body: `Open up this link to view your valet ticket: ${ticketLink}`,
+					to: phoneNumber,
+					from: twilioPhoneNumber
 				});
 
 				res.status(200);
 				res.send();
-				console.log(`ticket link is: ${ticket_link}`);
-				console.log('ticket sent to: ' + phone_number);
-				// fix this
+
 				fs.writeFile('sequencer.json', JSON.stringify({
-					"start": seq_no
+					'start': seqNo
 				}, null, 4));
 			}
-		}
-	});
+		};
 
-app.route('/user')
-	.get((req, res) => {
-		// TODO: decode from base64 first
-		const ticket_no = req.query.ticket;
-		const query = 'SELECT * FROM `users` WHERE ticket_no = ?';
-
+		// db manipulation
 		pool.getConnection((err, connection) => {
-			if(err) {
+			if (err) {
 				throw err;
 			}
-			connection.query(query, ticket_no, queryHandler);
+			connection.query(query, values, queryHandler);
 			connection.release();
 		});
 
+	});
+
+
+/**
+ * Endpoint to get user details
+ *
+ * @name GET/user
+ *
+ * @param {string} ticket - Ticket number generated for user
+ *
+ * Returns HTTP 200 OK status code
+ * Returns a JSON object containing user details
+ */
+app.route('/user')
+	.get((req, res) => {
+		// TODO: decode from base64 first
+		const ticketNum = req.query.ticket;
+		const query = 'SELECT * FROM `users` WHERE ticket_no = ?';
+
 		const queryHandler = (err, result) => {
-			if(err) {
-				// query failed
+			if (err) {
+				// query failed, handle exception
 				const errorModel = {
 					errorCode: 500,
 					errorMessage: 'Error executing the query on db',
@@ -139,16 +170,35 @@ app.route('/user')
 				res.status(200);
 				res.send(result[0]);
 			}
-		}
+		};
+
+		pool.getConnection((err, connection) => {
+			if (err) {
+				throw err;
+			}
+			connection.query(query, ticketNum, queryHandler);
+			connection.release();
+		});
+
 	});
 
+
+/**
+ * Endpoint which returns data needed for QR Code
+ * verificaton - to check if payment has been made
+ *
+ * @name GET/user/validation
+ *
+ * @param {string} ticket - Ticket number generated for user
+ *
+ * Returns HTTP 200 OK status code
+ * Returns a JSON object containing user data for validation
+ */
 app.route('/user/validation')
 	.get((req, res) => {
-		var qrcode = require('qrcode');
 		// TODO: base64 decoding first
-		const ticket_no = req.query.ticket;
+		const ticketNum = req.query.ticket;
 
-		// full_name, ticket_no, car_reg_no, car_model_make, payment_status, amount_paid
 		const query = 'SELECT full_name,\
 				ticket_no,\
 				car_reg_no,\
@@ -156,17 +206,9 @@ app.route('/user/validation')
 				amount_to_be_paid\
 			FROM `users` WHERE ticket_no = ?';
 
-		pool.getConnection((err, connection) => {
-			if(err) {
-				throw err;
-			}
-			connection.query(query, ticket_no, queryHandler);
-			connection.release();
-		});
-
 		const queryHandler = (err, result) => {
-			if(err) {
-				// query failed
+			if (err) {
+				// query failed, handle exception
 				const errorModel = {
 					errorCode: 500,
 					errorMessage: 'Error executing the query on db',
@@ -175,35 +217,50 @@ app.route('/user/validation')
 				console.log(errorModel);
 			} else {
 				const segments = [
-					{ 'data': `${result[0].car_reg_no}\n`, mode: 'byte' },
-					{ 'data': `${result[0].amount_to_be_paid}\n`, mode: 'byte' },
-					{ 'data': result[0].payment_status === 0 ? 'UNPAID' : 'PAID', mode: 'alphanumeric' },
+					{'data': `${result[0].car_reg_no}\n`, mode: 'byte'},
+					{'data': `${result[0].amount_to_be_paid}\n`, mode: 'byte'},
+					{
+						'data': result[0].payment_status === 0
+							? 'UNPAID'
+							: 'PAID',
+						mode: 'alphanumeric'
+					},
 				];
-				const qrObject = qrcode.create(segments);
 				res.status(200);
 				res.send(segments);
 			}
-		}
+		};
+
+		pool.getConnection((err, connection) => {
+			if (err) {
+				throw err;
+			}
+			connection.query(query, ticketNum, queryHandler);
+			connection.release();
+		});
+
 	});
 
+
+/**
+ * Endpoint which updates payment status when amount is paid
+ *
+ * @name POST/user/updatePaymentStatus
+ *
+ * @param {string} ticket - Ticket number generated for user
+ *
+ * Returns HTTP 200 OK status code on success
+ */
 app.route('/user/updatePaymentStatus')
 	.post((req, res) => {
 		// TODO: decode from base64 first
 		console.log(req.body.ticket);
-		const ticket_no = req.body.ticket;
+		const ticketNum = req.body.ticket;
 		const query = 'UPDATE `users` SET payment_status = 1 WHERE ticket_no = ?';
 
-		pool.getConnection((err, connection) => {
-			if(err) {
-				throw err;
-			}
-			connection.query(query, ticket_no, queryHandler);
-			connection.release();
-		});
-
-		const queryHandler = (err, result) => {
-			if(err) {
-				// query failed
+		const queryHandler = (err) => {
+			if (err) {
+				// query failed, handle exception
 				const errorModel = {
 					errorCode: 500,
 					errorMessage: 'Error executing the query on db',
@@ -214,5 +271,14 @@ app.route('/user/updatePaymentStatus')
 				res.status(200);
 				res.send();
 			}
-		}
+		};
+
+		pool.getConnection((err, connection) => {
+			if (err) {
+				throw err;
+			}
+			connection.query(query, ticketNum, queryHandler);
+			connection.release();
+		});
+
 	});
